979aefcf3874536e46599db33289444c83ecb67b
Taruna <taruna4@gmail.com>
2022-06-14 12:10:17 +0400

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+const  NewsItem =(props)=> {
+  
+    let {title, description,imageUrl, newsUrl,date,author,source}=props;
+    return (
+      <div className="my-3">
+        <div className="card" >
+          <div style={{display:"flex", justifyContent:"flex-end",position:"absolute",right:'0'}}>
+        <span className=" badge rounded-pill bg-danger" >
+   {source}
+    
+  </span>
+  </div>
+          <img src={!imageUrl?"https://images.indianexpress.com/2022/01/planet.jpg":imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknow":author} On {new Date(date).toGMTString()}</small></p>
+
+          
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">
+            Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+
+}
+
+export default NewsItem;
